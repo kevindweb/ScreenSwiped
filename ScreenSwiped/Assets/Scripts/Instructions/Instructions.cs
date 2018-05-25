dd4b757457a6376e0ab5794e65c2e78ecfe5fbba
Kevin Deems <kevindwebsites@gmail.com>
2018-05-25 11:37:55 -0400

    changes to instruction scene

---
+++ ScreenSwiped/Assets/Scripts/Instructions/Instructions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Instructions : MonoBehaviour {
+
+	public void ClickLoad(){
+		// go to load screen
+		SceneManager.LoadScene("LoadScene");
+	}
+}
