5503173be48858aa4c55a36730c280cdee0a034e
Kevin Deems <kevindwebsites@gmail.com>
2018-05-26 16:01:09 -0400

    Major updates to HUD with new item display

--- ScreenSwiped/Assets/Scripts/Game/HUD.cs
+++ ScreenSwiped/Assets/Scripts/Game/HUD.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
+	public GameObject itemHolder;
+	public GameObject itemParent;
+	public GameObject keyItem;
+	public GameObject tCanvas;
+	private GameObject[] platforms;
+	// list of usable platforms
 	private DataLoader access;
 	private float score = 0.0f;
 	private int highScore;
@@ -21,6 +28,16 @@ public class HUD : MonoBehaviour {
 			access.Save(difficulty, difficultyFile);
 		}
 		newHighScore = highScore;
+		float yPos = 2.0f;
+		int number = 1;
+		// set item list on left side of screen
+		CreateItem(Color.black, number.ToString(), yPos, itemParent.transform.position.x);
+		yPos -= itemHolder.transform.localScale.y + 0.3f;
+		number++;
+		CreateItem(Color.green, number.ToString(), yPos, itemParent.transform.position.x);
+		yPos -= itemHolder.transform.localScale.y + 0.3f;
+		number++;
+		CreateItem(Color.blue, number.ToString(), yPos, itemParent.transform.position.x);
 	}
 	void Update(){
 		float currPos = transform.position.x;
@@ -47,5 +64,21 @@ public class HUD : MonoBehaviour {
 		GUI.Label(new Rect(0, Screen.height - (2*h), scoreWidth, h), "Score: " + (int) score);
 		GUI.Label(new Rect(0, Screen.height - h, scoreWidth, h), "High Score: " + (int) newHighScore);
 		// put difficulty setting, score, and high score in bottom left of screen
+	}
+	void CreateItem(Color itemColor, string numberKey, float yPos, float xPos){
+		// create a visual to show player choice of items
+		// consists of outerbox, innerbox(color of platform), and a text element to show what key to press
+		GameObject item = Instantiate(itemHolder, new Vector3(xPos, yPos, 0), Quaternion.identity);
+		GameObject child = item.transform.GetChild(0).gameObject;
+		Renderer rend = child.GetComponent<Renderer>();
+		rend.material.SetColor("_Color", itemColor);
+		item.transform.parent = itemParent.transform;
+		GameObject number = Instantiate(keyItem, tCanvas.transform.position, Quaternion.identity);
+		number.transform.SetParent(tCanvas.transform);
+		Vector3 position = new Vector3(item.transform.position.x, item.transform.position.y - 0.3f, item.transform.position.z);
+		Vector3 point = Camera.main.WorldToScreenPoint(position);
+		number.transform.position = point;
+		Text ourText = number.GetComponent<Text>();
+		ourText.text = numberKey;
 	}
 }
