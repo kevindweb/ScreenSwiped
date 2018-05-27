cca7f624b1d58ce6d263f2b7e1b79d28b5edc955
Kevin Deems <kevindwebsites@gmail.com>
2018-05-26 21:42:19 -0400

    Added item view and platform expansion ability

--- ScreenSwiped/Assets/Scripts/Game/CreatePlatform.cs
+++ ScreenSwiped/Assets/Scripts/Game/CreatePlatform.cs
@@ -3,30 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CreatePlatform : MonoBehaviour {
-	public GameObject platform;
+	public GameObject[] platforms;
 	// static platform after being placed at certain position
-	public GameObject heldVersion;
+	public GameObject[] heldVersions;
 	// version of platform that uses script to follow mouse
 	private GameObject heldObj;
 	// used to destroy instantiated object following mouse
 	private bool holding = false;
 	// are we holding the object
-	void Update () {
+	private HUD hudScript;
+	private int currPlatformNum;
+	private Dictionary<KeyCode, int> keys = new Dictionary<KeyCode, int>();
+	void Start(){
+		hudScript = Camera.main.GetComponent<HUD>();
+		keys.Add(KeyCode.Alpha1, 1);
+		keys.Add(KeyCode.Alpha2, 2);
+		keys.Add(KeyCode.Alpha3, 3);
+	}
+	void Update(){
 		Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		mouseLocation.z = 0;
-		if(Input.GetKeyDown(KeyCode.Alpha1)){
-			// initialize platform at mouse location
-			if(heldObj){
-				Destroy(heldObj);
+		bool buttonClicked = false;
+		foreach(KeyValuePair<KeyCode, int> key in keys){
+    		if(Input.GetKeyDown(key.Key)){
+				buttonClicked = true;
+				// initialize platform at mouse location
+				if(heldObj){
+					Destroy(heldObj);
+				}
+				currPlatformNum = key.Value;
+				holding = true;
+				heldObj = Instantiate(heldVersions[currPlatformNum - 1], mouseLocation, Quaternion.identity);
+				hudScript.ChangeColor(currPlatformNum, true);
+				break;
 			}
-			holding = true;
-			heldObj = Instantiate(heldVersion, mouseLocation, Quaternion.identity);
-		} else if(Input.GetKeyDown(KeyCode.Escape) && holding){
+		}
+		if(Input.GetKeyDown(KeyCode.Escape) && holding && !buttonClicked){
 			// stop holding object and remove it from screen
 			Destroy(heldObj);
 			heldObj = null;
 			holding = false;
-		} else if(Input.GetKeyDown(KeyCode.Space) && holding){
+			hudScript.ChangeColor(currPlatformNum, false);
+		} else if(Input.GetKeyDown(KeyCode.Space) && holding && !buttonClicked){
 			// if holding, object follows mouse until we instantiate it with spacebar
 			FollowMouse script = heldObj.GetComponent<FollowMouse>();
 			float rotation = script.rotation;
@@ -37,11 +55,12 @@ public class CreatePlatform : MonoBehaviour {
 			}
 			Destroy(heldObj);
 			heldObj = null;
-			GameObject currPlatform = Instantiate(platform, mouseLocation, Quaternion.identity);
+			GameObject currPlatform = Instantiate(platforms[currPlatformNum - 1], mouseLocation, Quaternion.identity);
 			// instantiate actual static platform at location
 			PlatformSet newScript = currPlatform.GetComponent<PlatformSet>();
 			newScript.RotateTo(rotation);
 			holding = false;
+			hudScript.ChangeColor(currPlatformNum, false);
 		}
 
 	}
--- ScreenSwiped/Assets/Scripts/Game/HUD.cs
+++ ScreenSwiped/Assets/Scripts/Game/HUD.cs
@@ -8,7 +8,7 @@ public class HUD : MonoBehaviour {
 	public GameObject itemParent;
 	public GameObject keyItem;
 	public GameObject tCanvas;
-	private GameObject[] platforms;
+	private List<GameObject> platforms;
 	// list of usable platforms
 	private DataLoader access;
 	private float score = 0.0f;
@@ -18,6 +18,7 @@ public class HUD : MonoBehaviour {
 	private string difficultyFile = "difficulty.dat";
 	private string scoreFile = "score.dat";
 	private string prevScoreFile = "prevScore.dat";
+	private Color defaultItemBackground;
 	void Start(){
 		access = ScriptableObject.CreateInstance("DataLoader") as DataLoader;
 		difficulty = access.Load("", difficultyFile);
@@ -30,6 +31,9 @@ public class HUD : MonoBehaviour {
 		newHighScore = highScore;
 		float yPos = 2.0f;
 		int number = 1;
+		platforms = new List<GameObject>();
+		Renderer rendItem = itemParent.GetComponent<Renderer>();
+		defaultItemBackground = rendItem.material.color;
 		// set item list on left side of screen
 		CreateItem(Color.black, number.ToString(), yPos, itemParent.transform.position.x);
 		yPos -= itemHolder.transform.localScale.y + 0.3f;
@@ -69,6 +73,7 @@ public class HUD : MonoBehaviour {
 		// create a visual to show player choice of items
 		// consists of outerbox, innerbox(color of platform), and a text element to show what key to press
 		GameObject item = Instantiate(itemHolder, new Vector3(xPos, yPos, 0), Quaternion.identity);
+		platforms.Add(item);
 		GameObject child = item.transform.GetChild(0).gameObject;
 		Renderer rend = child.GetComponent<Renderer>();
 		rend.material.SetColor("_Color", itemColor);
@@ -80,5 +85,16 @@ public class HUD : MonoBehaviour {
 		number.transform.position = point;
 		Text ourText = number.GetComponent<Text>();
 		ourText.text = numberKey;
+		// place text inside the item on left side of screen
+	}
+	public void ChangeColor(int platformNum, bool selected){
+		// when selected, show player which platform is selected
+		GameObject platform = platforms[platformNum - 1];
+		Renderer rendItem = platform.GetComponent<Renderer>();
+		if(selected){
+			rendItem.material.SetColor("_Color", Color.blue);
+		} else{
+			rendItem.material.SetColor("_Color", defaultItemBackground);
+		}
 	}
 }
