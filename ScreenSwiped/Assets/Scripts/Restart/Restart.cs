7062f39a3aec30cc6012977763272d4089de3bdd
Kevin Deems <kevindwebsites@gmail.com>
2018-05-31 21:31:15 -0400

    Created high score celebration

--- ScreenSwiped/Assets/Scripts/Restart/Restart.cs
+++ ScreenSwiped/Assets/Scripts/Restart/Restart.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Restart : MonoBehaviour {
+	public GameObject explosive;
+	public float explosiveSpeed;
+	public int numExplosives = 1;
+	public int celebrationTime = 3;
+	public float startTime;
 	public Button backButton;
 	public Button restartButton;
 	public Font myFont;
@@ -18,7 +23,11 @@ public class Restart : MonoBehaviour {
 	private int currScore;
 	private string currScoreFile = "currScore.dat";
 	private bool isHighScore = false;
+	private GameObject[] explosives;
+	private Vector3[] forces;
+	private Color[] randomColors;
 	void Start(){
+		randomColors = new Color[] {Color.red, Color.blue, Color.yellow, Color.black, Color.white, Color.green};
 		myStyle = new GUIStyle();
 		myStyle.font = myFont;
 		myStyle.alignment = TextAnchor.MiddleCenter;
@@ -28,8 +37,32 @@ public class Restart : MonoBehaviour {
 		currScore = access.Load(0, currScoreFile);
 		if(highScore > prevScore)
 			isHighScore = true;
+		explosives = new GameObject[numExplosives];
+		forces = new Vector3[numExplosives];
+		startTime = Time.time;
+		for(int i=0; i < numExplosives; i++){
+			explosives[i] = Instantiate(explosive, new Vector3(0, 0, 13), Quaternion.identity);
+			explosives[i].GetComponent<Renderer>().material.color = randomColors[Random.Range(0, randomColors.Length - 1)];
+			forces[i] = RandomVector(-explosiveSpeed, explosiveSpeed);
+		}
 	}
+	private Vector3 RandomVector(float min, float max) {
+         var x = Random.Range(min, max);
+         var y = Random.Range(min, max);
+         return new Vector3(x, y, 0f);
+    }
 	void Update(){
+		if(Time.time - startTime < celebrationTime){
+			for(int i=0; i < numExplosives; i++){
+				Rigidbody2D rg2d = explosives[i].GetComponent<Rigidbody2D>();
+				rg2d.velocity = forces[i];
+			}
+		} else{
+			// destroy explosives
+			for(int i=0; i < numExplosives; i++){
+				Destroy(explosives[i]);
+			}
+		}
 		if(Input.GetKeyDown(KeyCode.Escape)){
 			// press back button
 			backButton.onClick.Invoke();
